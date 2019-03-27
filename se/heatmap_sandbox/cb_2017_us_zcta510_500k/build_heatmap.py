dabe87f3073ac23d103fa5b4718ddb1ed93e1cb7
seespinoza <se.espinoza132@gmail.com>
2019-03-27 00:17:48 -0500

    playing with maps

---
+++ se/heatmap_sandbox/cb_2017_us_zcta510_500k/build_heatmap.py
@@ -0,0 +1,36 @@
+"""
+Render a 2D map of Chicago with zip code boundaries.
+"""
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+import os
+import os.path
+
+
+us_shape_file_dir = "/home/sebastian/Documents/mcbi-datapalooza-2019/se/heatmap_sandbox/cb_2017_us_zcta510_500k"
+os.chdir(us_shape_file_dir)
+
+# Chicago coordinates.
+lowerlon = -69 
+upperlon = -80
+lowerlat = 24.5548
+upperlat = 45.5
+
+
+m = Basemap(
+    llcrnrlon=lowerlon,
+    llcrnrlat=lowerlat,
+    urcrnrlon=upperlon,
+    urcrnrlat=upperlat,
+    resolution='h',
+    projection='lcc',
+    lat_0=lowerlat,
+    lat_1=upperlat,
+    lon_0=lowerlon,
+    lon_1=upperlon
+    )
+
+
+shp_info = m.readshapefile(os.path.basename(us_shape_file_dir), 'state')
+plt.gca().axis("off")
+plt.show()
