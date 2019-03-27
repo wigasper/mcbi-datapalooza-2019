10e685eb46d0e94b54f28894f75a3a93cf33f1a7
seespinoza <se.espinoza132@gmail.com>
2019-03-27 17:45:51 -0500

    GA build_heat copy

---
+++ se/heatmap_sandbox/cb_2017_us_zcta510_500k/build_heatmap_v3.py
@@ -0,0 +1,101 @@
+from mpl_toolkits.basemap import Basemap
+from matplotlib.collections import LineCollection
+import matplotlib as mpl
+from matplotlib.colors import rgb2hex
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os
+
+
+# Read in population data.
+pop_path = "allGA.csv"
+DF = pd.read_csv(pop_path)
+colormap = plt.cm.Purples 
+
+us_shape_file_dir = "/home/sebastian/Documents/mcbi-datapalooza-2019/se/heatmap_sandbox/cb_2017_us_zcta510_500k"
+os.chdir(us_shape_file_dir)
+
+# Target area coordinates
+upperlon = -83
+lowerlon = -84
+lowerlat = 33
+upperlat = 34
+
+
+m = Basemap(
+        width=12000000,
+    height=12000000,
+    llcrnrlon=lowerlon,
+    llcrnrlat=lowerlat,
+    urcrnrlon=upperlon,
+    urcrnrlat=upperlat,
+    projection="lcc",
+    resolution="l",
+    lat_0=lowerlat,
+    lat_1=upperlat,
+    lon_0=lowerlon,
+    lon_1=upperlon
+    )
+
+
+shp_info = m.readshapefile(
+    os.path.basename(us_shape_file_dir),'states',drawbounds=True
+    )
+
+# Convert integer ZIP5 field to character dtype.
+DF['zip'] = DF['zip'].astype(str)
+
+# Read population density info into popdens dict. Take square root of 
+# actual density for better color mapping.
+popdens = {
+    str(i):j for (i, j) in zip(DF.zip.values,DF.score.values)
+    }
+
+# Choose a color for each state based on population density. Range
+# vmin-vmax has arbitrarily been set to 0-6. Fee lfree to experiment 
+# with other ranges.
+ziplist = []
+colors  = {}
+vmin    = 0.0
+vmax    = 1
+
+
+# Filter m.states_info to only Chicago zipcodes.
+zip_info   = m.states_info
+popdiv     = (max(popdens.values())/(vmax-vmin))
+popdensscl = {i:(j) for (i,j) in popdens.items()}
+
+
+for d in zip_info:
+    iterzip = d["ZCTA5CE10"]
+    if iterzip in popdensscl.keys():
+        iterpop = popdensscl.get(iterzip,0)
+        colors[iterzip] = colormap(iterpop/vmax)[:3]
+    ziplist.append(iterzip)
+
+
+for nshape,seg in enumerate(m.states):
+    i, j = zip(*seg)
+    if ziplist[nshape] in popdensscl.keys():
+        color = rgb2hex(colors[ziplist[nshape]])
+        edgecolor = "#000000"
+        plt.fill(i,j,color,edgecolor=edgecolor);
+
+m.drawrivers(linewidth=1, color='blue', linestyle = 'dashdot')
+
+# (Optional) include colorbar.
+sm = plt.cm.ScalarMappable(
+    cmap=colormap,norm=mpl.colors.Normalize(vmin=vmin, vmax=vmax)
+    )
+
+mm = plt.cm.ScalarMappable(cmap=colormap)
+mm.set_array([vmin, vmax])
+barObj = plt.colorbar(mm,orientation="vertical")
+barObj.set_ticks([0,.2,.4,.6,.8,1])
+barObj.ax.get_yaxis().labelpad = 15
+barObj.ax.set_ylabel('Potential Scores', rotation =270)
+
+plt.title("Potential Scores for Lawrenceville, GA Metro-Area Zip Codes")
+plt.gca().axis("off")
+plt.show()
